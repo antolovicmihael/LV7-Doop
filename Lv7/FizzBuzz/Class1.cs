0b92187f57afe7564471a12ad8c98bf6fdb276de
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-06-01 15:27:07 +0200

    Zadatci

---
+++ Lv7/FizzBuzz/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+
+
+    public class FizzBuzzer
+    {
+
+        public bool Check(string word)
+        {
+            if (string.IsNullOrEmpty(word)) throw new ArgumentException();
+
+
+            bool palindrom = false;
+            bool space = false;
+            bool upperLetter = false;
+            bool smallLetter = false;
+            //Task1
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - 1 - i])
+                {
+                    palindrom = true;
+                }
+                else
+                {
+                    palindrom = false;
+                }
+
+            }
+
+            //Task2
+            foreach (char c in word)
+            {
+                if (char.IsUpper(c)) upperLetter = false;
+                if (char.IsLower(c)) smallLetter = false;
+                if (char.IsWhiteSpace(c)) space = false;
+            }
+
+            //Task3
+            foreach (char c in word)
+            {
+                if (char.IsUpper(c)) char.ToLower(c);
+                if (char.IsPunctuation(c)) char.Parse(" ");
+                if (char.IsSymbol(c)) char.Parse("x");
+            }
+
+            return palindrom && upperLetter && smallLetter && space;
+
+
+        }
+
+        
+    }
+}
---
+++ Lv7/FizzBuzzTest/Class1.cs
@@ -0,0 +1,31 @@
+﻿using FizzBuzz;
+using NUnit.Framework;
+using System;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        //arrange
+        FizzBuzzer fizz = new FizzBuzzer();
+
+
+        [TestCase("aaaa")]
+        [TestCase("Palindrom")]
+        [TestCase("I am Batman")]
+        [TestCase("xxxxxxx")]
+        [TestCase(" Miho,  Miki.")]
+        [TestCase("x aaa .")]
+        public void Convert_Palindrom(string input)
+        {
+            bool expected = false;
+            //act
+            bool actual = fizz.Check(input);
+            //assrt
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+    }
+}
